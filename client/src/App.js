4277efdc71a9c762dab433362356cbc9be726b91
Achila Dilshan Abyesinghe <achila877@gmail.com>
2024-06-16 11:05:15 +0530

    Revert "frontend"

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,17 @@
 import React, { useState } from "react";
+
 import { BrowserRouter as Router, Route, Routes, Link, Navigate } from "react-router-dom";
-import Mainpage from './Pages/mainpage';
-import Loginpage from './Pages/login';
+import Loginpage from './Pages/Login';
 
 const App = () => {
 
   return (
     <Router>
       <div>
-        
           <Routes>
             <Route path="/" element={<Navigate to="/login" />} />
             <Route path="/login" element={<Loginpage />} />
             <Route path="/dashboard" element={<Mainpage />} />
-
           </Routes>
         
       </div>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,11 +4,7 @@ module.exports = {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {
-      boxShadow: {
-        'inner-custom': 'inset 0px -0.5px 2px rgba(244, 253, 255, 0.43)', 
-      }
-    },
+    extend: {},
   },
   plugins: [],
 };
